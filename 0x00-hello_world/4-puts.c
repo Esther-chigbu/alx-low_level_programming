b19a43b6eed28a1369a6ec5f0a721a57018019a0
Esther-chigbu <chigbuesther283@gmail.com>
2022-10-20 14:54:21 -0700

    First c challenge

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program that uses the print function
+ *
+ * Return: Always 0
+ */
+int main(void)
+
+{
+	puts("\"Programming ig is like building a multilingual puzzle");
+	return (0);
+}
