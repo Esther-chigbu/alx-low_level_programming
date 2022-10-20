5458ee262aeec75a91594e6fa3893592f7c61301
Esther-chigbu <chigbuesther283@gmail.com>
2022-10-20 15:09:09 -0700

    First c challenge

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - ENtry point
+ *
+ * Description: A C program that uses the printf function.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar , but the outcome is a picture of art, \n");
+	return (0);
+}
